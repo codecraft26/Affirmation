4a7e3b4de5ebf8fa26d2e1126df1c79fd980441e
codecraft26 <codescraft26@gmail.com>
2022-02-27 21:25:35 +0530

    initial commit

---
+++ app/src/main/java/dev/aman/affirmation/data/Datasource.kt
@@ -0,0 +1,21 @@
+package dev.aman.affirmation.data
+
+import dev.aman.affirmation.R
+import dev.aman.affirmation.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
